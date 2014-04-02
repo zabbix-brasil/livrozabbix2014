f5f75a56d548ebefe39c5fcf7939df5faf410512
spaww <the.spaww@gmail.com>
2014-04-02 10:16:58 -0300

    Instalação de pré-requisitos

---
+++ preRequisitos.sh
@@ -0,0 +1,6 @@
+apt-get -y install build-essential snmp libiksemel-dev vim libssh2-1-dev libssh2-1 libopenipmi-dev libsnmp-dev wget curl fping snmp-mibs-downloader apache2 php5 libapache2-mod-php5 php5-gd php-net-socket php5-ldap iotop ntp
+
+# Instalação de MySQL
+apt-get -y install php5-mysql mysql-server mysql-client libmysqld-dev 
+# Instalação de SQLite
+apt-get -y install php5-sqlite libsqlite3-dev sqlite3
