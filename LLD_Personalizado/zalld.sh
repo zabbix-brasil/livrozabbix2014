b5ce335bc0516a9a27373676c01c6e6d3274387d
spaww <the.spaww@gmail.com>
2014-04-24 08:00:12 -0300

    Template 803

--- LLD_Personalizado/zalld.sh
+++ LLD_Personalizado/zalld.sh
@@ -9,18 +9,18 @@
 # Version: 1.0
 #
  
-zaver="1.0"
-rval=0
- 
-function usage()
-{
-    echo "zadiskio version: $zaver $#"
-    echo "usage:"
-    echo "    LLD -- Retorna a lista de tabelas."
-    echo "    count,<TABELA> -- retorna a quantidade de linhas de uma tabela."
+zaver="1.0";
+rval=0;
+CAMINHO_FRONTEND="http://localhost/livro/livro-extras";
 
+usage() {
+    echo "zalld version: $zaver $#"
+    echo "Parametros:";
+    echo "    LLD -- Retorna a lista de tabelas.";
+    echo "    count,<TABELA> -- retorna a quantidade de linhas de uma tabela.";
+    echo "    version -- retorna a versao do script";
 }
- 
+
 ########
 # Main #
 ########
@@ -28,16 +28,21 @@ function usage()
 OPCAO=$1;
 case $OPCAO in
 'LLD')
-    curl http://localhost/extras/lldBasesMySQL.php?p_acao=LLD;
+    curl "$CAMINHO_FRONTEND/lldBasesMySQL.php?p_acao=LLD";
 ;;
 'count')
-    curl "http://localhost/extras/lldBasesMySQL.php?p_acao=count&p_tabela=$2";
+    curl "$CAMINHO_FRONTEND/lldBasesMySQL.php?p_acao=count&p_tabela=$2";
 ;;
+'version')
+    echo $zaver;
+;;
+*)
+    usage
+    exit $rval;;
 esac
- 
- 
-exit $rval
- 
-#
-# end 
 
+
+exit $rval
+
+#
+# end
