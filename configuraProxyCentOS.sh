2a7da1d8d3942bff3f57d639982d81eceeed9f22
SpawW <the.spaww@gmail.com>
2014-11-17 09:11:46 -0200

    Remoção

--- configuraProxyCentOS.sh
+++
@@ -1,9 +0,0 @@
-
-# Este capítulo utiliza como ambiente o zabbix 2.2.3
-SOURCE_DIR="/install/zabbix-2.2.3";
-cd $SOURCE_DIR
-
-cp $SOURCE_DIR/misc/init.d/fedora/core5/zabbix-server /etc/init.d/zabbix-proxy
-sed -i 's/server/proxy/g' /etc/init.d/zabbix-proxy
-chkconfig --add zabbix_proxy
-chkconfig --level 35 zabbix_proxy on
--- configuraProxyDebian.sh
+++
@@ -1,8 +0,0 @@
-
-# Este capítulo utiliza como ambiente o zabbix 2.2.3
-SOURCE_DIR="/install/zabbix-2.2.3";
-cd $SOURCE_DIR
-
-cp $SOURCE_DIR/misc/init.d/debian/zabbix-server /etc/init.d/zabbix-proxy
-sed -i 's/server/proxy/g' /etc/init.d/zabbix-proxy
-update-rc.d -f zabbix-proxy defaults
