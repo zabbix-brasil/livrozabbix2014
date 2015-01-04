d44fbc239d1db0903c147a05fe3897620ca46037
SpawW <the.spaww@gmail.com>
2015-01-03 22:20:44 -0200

    Capitulo 10

--- Capitulo_10/backupConfiguracao.sh
+++ Capitulo_10/backupConfiguracao.sh
@@ -1,5 +1,5 @@
 echo "host_trapper backup.inicio $(date)" > /tmp/logBackup.trap
-#tar -czvf /tmp/backupDados.tgz /var/www /usr/local/etc/zabbix*
+tar -czvf /tmp/backupDados.tgz /var/www /usr/local/etc/zabbix*
 TAMANHO=`du /tmp/backupDados.tgz | awk '{print $1}'`;
 echo "host_trapper backup.tamanho $TAMANHO" >> /tmp/logBackup.trap
 echo "host_trapper backup.fim $(date)" >> /tmp/logBackup.trap
