dea4f4539268cead7520d7999259a3c0c217805d
SpawW <the.spaww@gmail.com>
2015-01-26 16:55:40 -0200

    Configuracao do PHP

--- Capitulo_2/configuraPHP_Apache.sh
+++ Capitulo_2/configuraPHP_Apache.sh
@@ -1,12 +1,58 @@
-cp /etc/php.ini /etc/php.ini.ori;
-sed -i 's/max_execution_time/\;max_execution_time/g' /etc/php.ini;
-echo ' max_execution_time=300'>> /etc/php.ini;
-sed -i 's/max_input_time/\;max_input_time/g' /etc/php.ini;
-echo 'max_input_time=300' >> /etc/php.ini;
-sed -i 's/date.timezone/\;date.timezone/g' /etc/php.ini;
-echo ' date.timezone=America/Sao_Paulo' >> /etc/php.ini;
+#!/bin/bash
+#-------------------------------------------------------
+# author:       Adail Spinola <the.spaww@gmail.com>, Aecio Pires <aeciopires@gmail.com> e Andre Deo <andredeo@gmail.com>
+# date:         20-nov-2014
+# revision:     Aecio Pires <aecio@dynavideo.com.br>
+# Last updated: 21-jan-2015, 18:08
+#-------------------------------------------------------
 
-sed -i 's/post_max_size/\;post_max_size/g' /etc/php.ini;
-echo ' post_max_size=16M' >> /etc/php.ini;
+CMDLINE=$0
+
+# Detecta se eh um usuario com poderes de root que esta executando o script
+MYUID=$(id | cut -d= -f2 | cut -d\( -f1)
+if [ ! "$MYUID" -eq 0 ] ; then
+        echo "voce deve ser root para executar este script."
+        echo "execute o comando \"sudo $CMDLINE\""
+        exit 1
+fi
+
+#-----------------------------------------------------------
+# comment: Obtem o nome da distribuicao
+# syntax: LINUXDISTRO=$(get_linux_distro)
+# return: $LINUXDISTRO contem o nome da distro
+#
+get_linux_distro(){
+LINUXDISTRO=UNKNOWN
+
+# Obtendo o codnome da distro
+grep "Debian" /etc/issue > /dev/null 2>&1 && LINUXDISTRO=DEBIAN
+grep "CentOS" /etc/redhat-release > /dev/null 2>&1 && LINUXDISTRO=CENTOS
+grep "Red Hat" /etc/redhat-release > /dev/null 2>&1 && LINUXDISTRO=REDHAT
+grep "Ubuntu" /etc/issue > /dev/null 2>&1 && LINUXDISTRO=UBUNTU
+
+echo $LINUXDISTRO
+}
+
+LINUXDISTRO=$(get_linux_distro)
+
+case "$LINUXDISTRO" in
+        CENTOS|REDHAT)
+		PHPFILE=/etc/php.ini
+        ;;
+        DEBIAN|UBUNTU)
+		PHPFILE=/etc/php5/apache2/php.ini
+        ;;
+esac
+
+cp $PHPFILE $PHPFILE.ori;
+sed -i 's/max_execution_time/\;max_execution_time/g' $PHPFILE;
+echo ' max_execution_time=300'>> $PHPFILE;
+sed -i 's/max_input_time/\;max_input_time/g' $PHPFILE;
+echo 'max_input_time=300' >> $PHPFILE;
+sed -i 's/date.timezone/\;date.timezone/g' $PHPFILE;
+echo ' date.timezone=America/Sao_Paulo' >> $PHPFILE;
+
+sed -i 's/post_max_size/\;post_max_size/g' $PHPFILE;
+echo ' post_max_size=16M' >> $PHPFILE;
 
 service apache2 restart
