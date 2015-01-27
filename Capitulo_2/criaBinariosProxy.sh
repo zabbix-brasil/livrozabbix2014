ba9ea311515d7cd5eb2b8d9198e54cbdd5834b06
SpawW <the.spaww@gmail.com>
2015-01-27 10:40:04 -0200

    Cria os binários do Proxy

--- Capitulo_2/criaBinariosProxy.sh
+++ Capitulo_2/criaBinariosProxy.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#-------------------------------------------------------
+# author:       Adail Spinola <the.spaww@gmail.com>, Aecio Pires <aeciopires@gmail.com> e Andre Deo <andredeo@gmail.com>
+# date:         20-nov-2014
+# revision:     Aecio Pires <aecio@dynavideo.com.br>
+# Last updated: 21-jan-2015, 18:08
+#-------------------------------------------------------
+
+CMDLINE=$0
+
+# Detecta se eh um usuario com poderes de root que esta executando o script
+MYUID=$(id | cut -d= -f2 | cut -d\( -f1)
+if [ ! "$MYUID" -eq 0 ] ; then
+        echo "voce deve ser root para executar este script."
+        echo "execute o comando \"sudo $CMDLINE\""
+        exit 1
+fi
 
 # Este capítulo utiliza como ambiente o zabbix 2.*
 SOURCE_DIR="/install/zabbix-2*";
