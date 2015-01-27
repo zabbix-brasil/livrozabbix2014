e9bc68708937904ac75682d8eb6b4b19759985d8
SpawW <the.spaww@gmail.com>
2015-01-27 10:52:57 -0200

    Configuração do proxy

--- Capitulo_13/old/configuraProxy.sh
+++ Capitulo_13/old/configuraProxy.sh
@@ -1,21 +1,3 @@
-#!/bin/bash
-#-------------------------------------------------------
-# author:       Adail Spinola <the.spaww@gmail.com>, Aecio Pires <aeciopires@gmail.com> e Andre Deo <andredeo@gmail.com>
-# date:         20-nov-2014
-# revision:     Aecio Pires <aecio@dynavideo.com.br>
-# Last updated: 21-jan-2015, 18:08
-#-------------------------------------------------------
-
-CMDLINE=$0
-
-# Detecta se eh um usuario com poderes de root que esta executando o script
-MYUID=$(id | cut -d= -f2 | cut -d\( -f1)
-if [ ! "$MYUID" -eq 0 ] ; then
-        echo "voce deve ser root para executar este script."
-        echo "execute o comando \"sudo $CMDLINE\""
-        exit 1
-fi
-
 ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
 
 if [ -f /etc/lsb-release ]; then
