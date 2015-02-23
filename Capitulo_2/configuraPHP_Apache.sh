21f2cc48dcb874a711822a222ffb69a0dff0bf69
SpawW <the.spaww@gmail.com>
2015-02-23 19:06:54 -0300

    Capitulo 2

--- Capitulo_2/configuraPHP_Apache.sh
+++ Capitulo_2/configuraPHP_Apache.sh
@@ -4,6 +4,8 @@
 # date:         20-nov-2014
 # revision:     Aecio Pires <aecio@dynavideo.com.br>
 # Last updated: 21-jan-2015, 18:08
+# revision:     Andre Deo <andredeo@gmail.com>
+# Last updated: 27-jan-2015, 23:35
 #-------------------------------------------------------
 
 CMDLINE=$0
@@ -55,4 +57,12 @@ echo ' date.timezone=America/Sao_Paulo' >> $PHPFILE;
 sed -i 's/post_max_size/\;post_max_size/g' $PHPFILE;
 echo ' post_max_size=16M' >> $PHPFILE;
 
-service apache2 restart
+
+case "$LINUXDISTRO" in
+        CENTOS|REDHAT)
+		service httpd restart
+        ;;
+        DEBIAN|UBUNTU)
+		service apache2 restart
+        ;;
+esac
