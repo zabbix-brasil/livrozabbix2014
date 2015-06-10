963c2e881e3dae1606178910c8385a7eb769699b
spaww <the.spaww@gmail.com>
2015-06-10 14:54:45 -0300

    Capítulo 14

--- Capitulo_13/configuraProxy.sh
+++ Capitulo_13/configuraProxy.sh
@@ -40,33 +40,62 @@ SOURCE_DIR="/install/zabbix-2.*";
 cd $SOURCE_DIR
 
 case $OS in
-   Debian)
-     echo "Instalando em debian";
-     apt-get install -y --force-yes sqlite3 libsqlite3-dev php5-sqlite;
-cp $SOURCE_DIR/misc/init.d/debian/zabbix-server /etc/init.d/zabbix-proxy
-sed -i 's/server/proxy/g' /etc/init.d/zabbix-proxy
-sed -i 's/Server/Proxy/g' /etc/init.d/zabbix-proxy
-update-rc.d -f zabbix-proxy defaults
+   Debian|Ubuntu)
+	echo "Instalando o SQLite no Debian/Ubuntu";
+	apt-get install -y --force-yes sqlite3 libsqlite3-dev php5-sqlite;
+	cp $SOURCE_DIR/misc/init.d/debian/zabbix-server /etc/init.d/zabbix-proxy
+	sed -i 's/server/proxy/g' /etc/init.d/zabbix-proxy
+	sed -i 's/Server/Proxy/g' /etc/init.d/zabbix-proxy
+	update-rc.d -f zabbix-proxy defaults
    ;;
    CentOS)
-     echo "Instalando em CentOS";
-     yum -y install  sqlite sqlite-devel 
-cp ./misc/init.d/fedora/core5/zabbix_server /etc/init.d/zabbix_proxy
-sed -i 's/server/proxy/g' /etc/init.d/zabbix_proxy
-sed -i 's/Server/Proxy/g' /etc/init.d/zabbix_proxy
-chkconfig --add zabbix_proxy
-chkconfig --level 35 zabbix_proxy on
+	echo "Instalando o SQLite no CentOS";
+	yum -y install  sqlite sqlite-devel 
+	cp ./misc/init.d/fedora/core5/zabbix_server /etc/init.d/zabbix_proxy
+	sed -i 's/server/proxy/g' /etc/init.d/zabbix_proxy
+	sed -i 's/Server/Proxy/g' /etc/init.d/zabbix_proxy
+	chkconfig --add zabbix_proxy
+	chkconfig --level 35 zabbix_proxy on
    ;;
    *)
-     echo "Distro ($OS) nao reconhecida!";
+	echo "Distro ($OS) nao reconhecida!";
    ;;
 esac
+
+echo "Compilando o Zabbix Proxy..."
 ./configure --enable-proxy --enable-agent --with-sqlite3 --with-net-snmp  --with-libcurl 
 make install
+
+echo "Criando o banco de dados do SQLIte, caso nao exista..."
 if [ ! -d /var/lib/sqlite/ ]; then 
-  mkdir /var/lib/sqlite/
+  mkdir /var/lib/sqlite/ > /dev/null 2>&1
 fi 
 if [ ! -f /var/lib/sqlite/zabbix.db ]; then
    sqlite3 /var/lib/sqlite/zabbix.db < ./database/sqlite3/schema.sql
 fi 
 chown -R zabbix:zabbix /var/lib/sqlite/
+
+CONF_PROXY=/usr/local/etc/zabbix_proxy.conf
+DB_SQLITE=/var/lib/sqlite/zabbix.db
+
+USUARIODB="zabbix";
+
+mv $CONF_PROXY $CONF_PROXY.ori.$$
+echo "DBName=$DB_SQLITE" > $CONF_PROXY
+echo "DBUser=$USUARIODB" >> $CONF_PROXY
+echo "ConfigFrequency=120" >> $CONF_PROXY
+
+echo "DebugLevel=3" >> $CONF_PROXY
+echo "Hostname=proxy_filial" >> $CONF_PROXY
+echo "Server=localhost" >> $CONF_PROXY
+echo "ServerPort=10051" >> $CONF_PROXY
+echo "ListenPort=10053" >> $CONF_PROXY
+echo "PidFile=/tmp/zabbix_proxy.pid" >> $CONF_PROXY
+echo "LogFile=/tmp/zabbix_proxy.log" >> $CONF_PROXY
+echo "Timeout=3" >> $CONF_PROXY
+
+PATH_FPING=$(which fping);
+echo "FpingLocation=$PATH_FPING" >> $CONF_PROXY
+
+echo "O arquivo de configuracao do Zabbix Proxy esta em $CONF_PROXY"
+
